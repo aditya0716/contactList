d4f8ea285c50e0456ccdd2e692d914c2c4d1bd3a
aditya0716 <aditya.shandilya16798@gmail.com>
2020-11-12 02:09:53 +0530

    Add_Contact

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const port = 8000;
+const app = express();
+app.use(express.urlencoded());
+app.use(express.static("assets"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+var contactList = [
+  {
+    name: "Tony",
+    phone: "5674390876",
+  },
+  {
+    name: "Cap",
+    phone: "1234567890",
+  },
+  {
+    name: "Thor",
+    phone: "8756465518",
+  },
+];
+
+app.get("/", function (req, res) {
+  return res.render("home", {
+    title: "MY Contacts-List",
+    contact_list: contactList,
+  });
+});
+app.get("/practice", function (req, res) {
+  return res.render("practice", { title: "Let us play with ejs" });
+});
+app.post("/create-contact", function (req, res) {
+  // contactList.push({ name: req.body.name, phone: req.body.phone });
+  contactList.push(req.body);
+  return res.redirect("back");
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("ERROR IN RUMMING:", err);
+    return;
+  }
+  console.log("EXPRESS SERVER IS RUNNING ON PORT:", port);
+});
